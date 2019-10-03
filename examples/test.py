1ce124dbfabf75412913ae39db6358fee88b6221
Meilof Veeningen <meilof@gmail.com>
2019-10-03 12:04:19 +0200

    fix clashes with swig

--- examples/test.py
+++ examples/test.py
@@ -1,31 +1,31 @@
 import libsnark
 
-pb=libsnark.protoboard_pub()
+pb=libsnark.zks_protoboard_pub()
 
 # create variables
 
-inv=libsnark.pb_variable()
+inv=libsnark.zks_pb_variable()
 inv.allocate(pb)
 pb.setpublic(inv)
 
-int=libsnark.pb_variable()
+int=libsnark.zks_pb_variable()
 int.allocate(pb)
 
-outv=libsnark.pb_variable()
+outv=libsnark.zks_pb_variable()
 outv.allocate(pb)
 pb.setpublic(outv)
 
 # create constraints
 
 # let int=inv*(2*inv+1)
-pb.add_r1cs_constraint(libsnark.r1cs_constraint(libsnark.linear_combination(inv),
-                                                libsnark.linear_combination(inv)*2+libsnark.linear_combination(1),
-                                                libsnark.linear_combination(int)))
+pb.add_r1cs_constraint(libsnark.r1cs_constraint(libsnark.zks_linear_combination(inv),
+                                                libsnark.zks_linear_combination(inv)*2+libsnark.zks_linear_combination(1),
+                                                libsnark.zks_linear_combination(int)))
                        
 # let out=(int-1)*inv
-pb.add_r1cs_constraint(libsnark.r1cs_constraint(libsnark.linear_combination(int)-libsnark.linear_combination(1),
-                                                libsnark.linear_combination(inv),
-                                                libsnark.linear_combination(outv)))
+pb.add_r1cs_constraint(libsnark.r1cs_constraint(libsnark.zks_linear_combination(int)-libsnark.zks_linear_combination(1),
+                                                libsnark.zks_linear_combination(inv),
+                                                libsnark.zks_linear_combination(outv)))
 
 # create witnesses
 pb.setval(inv, 3)
@@ -40,7 +40,7 @@ print("*** Trying to read key")
 keypair=libsnark.read_key("ekfile", cs)
 if not keypair:
     print("*** No key or computation changed, generating keys...")
-    keypair=libsnark.r1cs_ppzksnark_generator(cs)
+    keypair=libsnark.zks_generator(cs)
     libsnark.write_keys(keypair, "vkfile", "ekfile")
     
 print("*** Generating proof (" +
@@ -50,8 +50,8 @@ print("*** Generating proof (" +
       ", #constraint=" + str(pb.num_constraints()) +
        ")")
     
-proof=libsnark.r1cs_ppzksnark_prover(keypair.pk, pubvals, privvals);
-verified=libsnark.r1cs_ppzksnark_verifier_strong_IC(keypair.vk, pubvals, proof);
+proof=libsnark.zks_prover(keypair.pk, pubvals, privvals);
+verified=libsnark.zks_verifier_strong_IC(keypair.vk, pubvals, proof);
     
 print("*** Public inputs: " + " ".join([str(pubvals.at(i)) for i in range(pubvals.size())]))
 print("*** Verification status:", verified)
