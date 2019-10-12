5be131c0c3f8b1d1e3e8f5c5da3cacd393834d03
Meilof Veeningen <meilof@gmail.com>
2019-10-12 20:43:27 +0200

    setup

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Python3 bindings for libsnark.
 
-Assumes libsnark is built with `-DBINARY_OUTPUT=ON -DMONTGOMERY_OUTPUT=ON -DUSE_PT_COMPRESSION=OFF` and installed.
+Assumes [this libsnark branch](https://github.com/meilof/libsnark) is built with `cmake -DCURVE=ALT_BN128 -DUSE_PT_COMPRESSION=OFF -DWITH_PROCPS=OFF` and installed. If libsnark is built with different flags, `setup.py`'s `extra_compile_flags` should be adapted.
 
 Install with:
 
@@ -10,3 +10,16 @@ python setup.py install
 
 The wrapper is created using SWIG. To update the wrapper, use `swig -python -c++ -o alt_bn128_wrap.cpp libsnark.i`.
 
+Under Linux, the following sequence of commands can be used to install libsnark:
+
+```
+git clone --recursive https://github.com/meilof/libsnark
+cd libsnark/
+mkdir build
+cd build/
+cmake .. -DCURVE=ALT_BN128 -DUSE_PT_COMPRESSION=OFF -DWITH_PROCPS=OFF
+make
+sudo make install
+```
+
+For Mac OS, flags like `-DCMAKE_PREFIX_PATH=/usr/local/Cellar/openssl/1.0.2t -DCMAKE_SHARED_LINKER_FLAGS=-L/usr/local/Cellar/openssl/1.0.2t/lib` can be added.
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # setup.py
+import setuptools
 import distutils
 from distutils.core import setup, Extension
 import os
@@ -8,13 +9,15 @@ print("* To update the wrapper, use swig -python -c++ -o alt_bn128_wrap.cpp libs
 
 # cannot use .i file below because distutils doesn't understand c++ wrappers
 # USE_ASM breaks stuff on mac os
+# , extra_link_args=['-static'] on macos makes sense?
 
 setup(name = "python-libsnark",
-      version = "0.3",
+      version = "0.3.1",
       description='Python bindings for a restricted subset of libsnark',
       author='Meilof Veeningen',
       author_email='meilof@gmail.com',
+      license='MIT',
       url='https://github.com/meilof/python-libsnark',
       packages = ["libsnark"],
       package_dir = {"libsnark": "."},
-      ext_modules = [Extension("libsnark._alt_bn128", ["alt_bn128_wrap.cpp"], extra_compile_args=["-std=c++11", "-Wno-sign-compare", "-Wno-delete-non-virtual-dtor", "-Wno-unused-variable", "-DCURVE_ALT_BN128", "-DBINARY_OUTPUT", "-DBN_SUPPORT_SNARK=1", "-DMONTGOMERY_OUTPUT", "-DNO_PROCPS"], libraries=["snark", "ff", "gmpxx"], extra_link_args=['-static'], swig_opts=["-c++"])])
+      ext_modules = [Extension("libsnark._alt_bn128", ["alt_bn128_wrap.cpp"], extra_compile_args=["-std=c++11", "-Wno-sign-compare", "-Wno-delete-non-virtual-dtor", "-Wno-unused-variable", "-DCURVE_ALT_BN128", "-DBINARY_OUTPUT", "-DBN_SUPPORT_SNARK=1", "-DMONTGOMERY_OUTPUT", "-DNO_PROCPS"], libraries=["snark", "ff", "gmpxx"], swig_opts=["-c++"])])
